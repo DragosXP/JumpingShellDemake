820a2dd07472d5920c1a044cfb9cc6ddee999dbe
DragosXP <dragosxp715@gmail.com>
2025-12-23 12:58:23 +0200

    Added main window and sprites

--- src/main/java/com/pixelrent/jumpingshelldemake/JumpingShellDemake.java
+++ src/main/java/com/pixelrent/jumpingshelldemake/JumpingShellDemake.java
@@ -1,9 +1,26 @@
 package com.pixelrent.jumpingshelldemake;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import java.awt.Color;
 
 public class JumpingShellDemake {
 
     public static void main(String[] args) {
-        System.out.println("Hello  h");
+        SwingUtilities.invokeLater(() -> {
+            createAndShowGUI();
+        });
+    }
+    
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Jumping Shell Demake");
+
+        frame.setSize(1280, 960);
+
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(Color.decode("#1f102a"));
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }

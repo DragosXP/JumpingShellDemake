17234199f1bb9951f92ff103a1781434d9507ae6
DragosXP <dragosxp715@gmail.com>
2025-12-23 12:39:02 +0200

    I added the sprites

--- src/main/java/com/pixelrent/jumpingshelldemake/JumpingShellDemake.java
+++ src/main/java/com/pixelrent/jumpingshelldemake/JumpingShellDemake.java
@@ -4,6 +4,6 @@ package com.pixelrent.jumpingshelldemake;
 public class JumpingShellDemake {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello  h");
     }
 }
